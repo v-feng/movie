015e0e604eba0909a5248df73771db13c83c16aa
wb-mwf606729 <wb-mwf606729@gongdao.com>
2019-11-07 10:38:02 +0800

    tree

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const Header = (props) =>{
+
+    return (
+    <header className="App-header">
+        <h2>{props.text}</h2>
+    </header>
+    )
+}
+export default Header;
